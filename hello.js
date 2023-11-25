9f19686fea187280ad387a05c78da316fb676d1a
jack <juesc007@gmail.com>
2023-11-25 16:58:14 +0900

    first commit

--- hello.js
+++ hello.js
@@ -5,13 +5,53 @@ const app = express()
 
 const port = 8888
 
+let users = []; 
+let feeds = []; 
 
-const user = (req, res) => {
+/**
+ * Entry API
+ */
+const entry = (req, res) =>{
+    // urlInfo = url.parse(req.url, true);
+    res.end("Hello world!")
+}
+/**
+ * User APIs 
+ */
+function _userExists(id) {
+    console.log(users)
+    console.log(users[id])
+    if (users[id]) return true;
+    return false
+}
+const getUser = (req, res) => {
     const userInfo = url.parse(req.url, true).query
-    res.end(`[user] name: ${userInfo.name}, age: ${userInfo.age}`)
+    if (_userExists(userInfo.id)) {
+        res.end(users[userInfo.id])
+    } else {
+        res.end(`User not found with such Id.`)
+    }
 }
 
-const feed = (req, res) => {
+const createUser = (req,res) => {
+    const userInfo = url.parse(req.url, true).query
+    users.append({"name": userInfo.name, "age": userInfo.age});
+    res.end("User created.")
+}
+
+const deleteUser = (req, res) => {
+    const userId = url.parse(req.url, true).query
+    if (_userExists(userId)) {
+        delete users[userId]
+        res.end(`User ${userId} deleted.`)
+    } else {
+        res.end("User not found with such Id.")
+    }
+}
+/**
+ * Feed APIs
+ */
+const getFeed = (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"})
     res.end(`
     <ul>
@@ -25,7 +65,10 @@ const notFound = (req,res)=>{
     res.end("404 Not Found");
 }
 
+app.get("/", entry)
+app.get("/user", getUser)
+app.post("/user", createUser)
+app.delete("/user/:id", deleteUser)
+app.get("/feed", getFeed)
 
-app.get("/user", user)
-app.get("/feed", feed)
 app.listen(port, ()=> console.log(`Server started on ${port}`))
