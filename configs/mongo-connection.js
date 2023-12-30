47d28f391a4fa7152d20e248ca8c8c8b1e3f8572
yesj1234 <juesc007@gmail.com>
2023-12-30 10:38:26 +0900

    Clean main branch for a fresh start

--- configs/mongo-connection.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config({ path: "../.env" });
-const { MongoClient } = require("mongodb");
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-const uri = `mongodb+srv://yesj1234:${MONGODB_PASSWORD}@cluster0.wmwqk54.mongodb.net/?retryWrites=true&w=majority`;
-module.exports = async function (callback) {
-  return await MongoClient.connect(uri, callback);
-};
