beb03deac46acdac066641b02cc51bb402d9ca1f
jack <juesc007@gmail.com>
2024-01-07 16:37:57 +0900

    initial commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
+import { SessionSerializer } from './session.serializer';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserService],
+  imports: [UserService, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, SessionSerializer, LocalStrategy],
 })
 export class AuthModule {}
