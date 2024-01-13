fe6a29c6675e9fc89c0c11c17cc1d89405b2a12d
yesj1234 <juesc007@gmail.com>
2024-01-13 14:23:11 +0900

    .

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
-import { AuthService } from './auth.service';
+import { User } from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
+import { Profile, Strategy } from 'passport-google-oauth20';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super({ usernameField: 'email' });
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google',
+      scope: ['email', 'profile'],
+    });
   }
 
-  async validate(email: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(email, password);
-    if (!user) {
-      return null;
-    }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+  ): Promise<any> {
+    const { id, name, emails } = profile;
+    const providerId = id;
+    const email = emails[0].value;
+    const user: User = await this.userService.findByEmailOrSave(
+      email,
+      name.familyName + name.givenName,
+      providerId,
+    );
     return user;
   }
 }
